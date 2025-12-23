286d9ee34806b1b9e43562d501080333cf38f02f
Akuthota Suprathika <suprathikaakuthota@gmail.com>
2025-12-23 20:33:55 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,20 @@
+public class arr {
+    public static void main(String[] args) {
+        String[] names={"Chinni","chinnu","keerthi","varshitha","aqsa","satvhika","kavya","sajani","umesh","saicharan"};
+System.out.println("current first name:");
+System.out.println(names[1]);
+System.out.println("updating first name:");   
+names[1]="ranaveer";
+System.out.println(names[1]);
+{int[] arr = new int[100];
+        
+        for(int i = 0; i < 100; i++) {
+            arr[i] = 3 * (i + 1);
+        }
+        
+        for(int i = 0; i < 100; i++) {
+            System.out.print(arr[i] + " ");
+        }
+}
+}
+}
